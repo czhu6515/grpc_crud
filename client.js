4382f43017f183bd597290fb43de327ed3da2e96
Chenyu Zhu <somewhatalive@gmail.com>
2020-02-17 14:30:30 -0500

    working branch

--- client.js
+++ client.js
@@ -1,27 +1,20 @@
 const grpcLoader = require('@grpc/proto-loader')
 const grpc = require('grpc')
-const readline = require("readline");
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
 
-const PROTO_PATH = './notes.proto'
+const options = {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true
+}
 
-const NoteService = grpcLoader.load(PROTO_PATH).NoteService
+const NotesDefinition = grpcLoader.loadSync('notes.proto', options);
+const NoteService = grpc.loadPackageDefinition(NotesDefinition).NoteService;
+
 const client = new NoteService('localhost:8008', 
     grpc.credentials.createInsecure())
 
 
-
-rl.question('cake or death', () => {
-    console.log('cake')
-    rl.close()
-})
-
-rl.on('close', () => {
-    console.log('later alligator')
-    process.exit(0)
-})
 
 module.exports = client
--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const options = {
     longs: String,
     enums: String,
     defaults: true,
-    oneofs: true
+    oneofs: true,
+    arrays: true
 }
 
 const NotesDefinition = protoLoader.loadSync('notes.proto', options);
@@ -17,12 +18,12 @@ const notes = [
     { id: '1', title: 'Note 1', content: 'Content 1'},
     { id: '2', title: 'Note 2', content: 'Content 2'}
 ]
-
+console.log(notes)
 const server = new grpc.Server()
 
 server.addService(notesProto.NoteService.service, {
     list: (_, callback) => {
-        callback(null, notes)
+        callback(null, "notes")
     },
     insert: (call, callback) => {
         let note = call.request
